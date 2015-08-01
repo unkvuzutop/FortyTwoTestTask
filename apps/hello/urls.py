3c28d9d3cae25f02a9aaceefee59f137c2025fb0
unkvuzutop <inkvizitor.dev@gmail.com>
2015-08-01 17:30:11 +0300

    static files try fix

--- apps/hello/urls.py
+++ apps/hello/urls.py
@@ -5,13 +5,6 @@ import views
 
 urlpatterns = [
     url(r'^$', views.user_detail, name='home'),
-    # url(r'^products/$', views.products, name='products'),
-    # url(r'^products/(?P<slug>[\w-]+)/$', views.product_detail,
-    #     name='product_detail'),
-    # url(r'^signup/$', views.sign_up, name='sign_up'),
-    # url(r'^signin/$', views.sign_in, name='sign_in'),
-    # url(r'^signout/$', views.sign_out, name='sign_out'),
-    # url(r'^like/(?P<product_id>[0-9]+)$', views.like_product, name='like')
 ]
 
 urlpatterns += staticfiles_urlpatterns()
--- fortytwo_test_task/settings/__init__.py
+++ fortytwo_test_task/settings/__init__.py
@@ -1,5 +1,5 @@
 from .common import *  # flake8: noqa
 try:
-    from .local import *  # flake8: noqa
+        from .local import *  # flake8: noqa
 except ImportError:
     pass
