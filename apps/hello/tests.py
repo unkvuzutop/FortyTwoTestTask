38daf547b5c4898fb2a1a0e5992c869ce790867e
unkvuzutop <inkvizitor.dev@gmail.com>
2015-08-01 17:00:22 +0300

    ticket:1 tests pass

--- apps/hello/tests.py
+++ apps/hello/tests.py
@@ -5,5 +5,5 @@ from django.test import TestCase
 
 class SomeTests(TestCase):
     def test_math(self):
-        "put docstrings in your tests"
+        "put docstrings in your te2sts"
         assert(2 + 2 == 4)
