6366a6cafe59c1bf823ad5f7012b729df9fd0940
unkvuzutop <inkvizitor.dev@gmail.com>
2015-08-10 00:40:53 +0300

    ticket:8  add template tag

---
+++ apps/hello/templatetags/admin_tags.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from django.core import urlresolvers
+from django.contrib.contenttypes.models import ContentType
+from django import template
+
+register = template.Library()
+
+
+@register.simple_tag
+def admin_url(instance):
+    try:
+        content_type = ContentType.objects.get_for_model(instance.__class__)
+        return '<a class="btn btn-primary" href="'\
+               + urlresolvers.reverse("admin:%s_%s_change" %
+                                      (content_type.app_label,
+                                       content_type.model),
+                                      args=(instance.id,))\
+               + '"><span  ' \
+                 'aria-hidden="true"></span>Admin</a>'
+    except:
+        return ""
