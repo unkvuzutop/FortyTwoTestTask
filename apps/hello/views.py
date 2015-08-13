b4b753223e7d3014f938f93861247e5b6da628a0
unkvuzutop <inkvizitor.dev@gmail.com>
2015-08-14 00:18:45 +0300

    ticket:3 add filter for ajax requests for request page

--- apps/hello/views.py
+++ apps/hello/views.py
@@ -26,9 +26,15 @@ def request_list(request):
         .order_by('-date')[:10]
     last_request = RequestHistory.objects.latest('id')
 
+    latest_requests_count = 0
+    for request in latest_requests:
+        if not request.is_viewed:
+            latest_requests_count += 1
+
     return render_to_response('hello/requests.html',
                               {'latest_requests': latest_requests,
-                               'last_request': last_request})
+                               'last_request': last_request,
+                               'latest_requests_count': latest_requests_count})
 
 
 @csrf_protect
@@ -38,6 +44,8 @@ def ajax_update(request):
         try:
             result = RequestHistory.objects\
                 .filter(id__in=viewed_request_id.split(','))\
+                .exclude(path__in=[reverse('hello:ajax_update'),
+                         reverse('hello:ajax_count')])\
                 .update(is_viewed=True)
         except Exception as e:
             logging.info('can\'t update object')
@@ -62,6 +70,8 @@ def ajax_count(request):
         requests = RequestHistory.objects\
             .filter(id__gt=request.POST['last_loaded_id'])\
             .filter(is_viewed=False)\
+            .exclude(path__in=[reverse('hello:ajax_update'),
+                     reverse('hello:ajax_count')])\
             .all()
 
         last_request = RequestHistory.objects.latest('id')
