faef9cb4aeed59ce66baab2f787108fefe5e1226
unkvuzutop <inkvizitor.dev@gmail.com>
2015-08-27 00:22:37 +0300

    ticket:3  decorate ajax and refactoring after review

--- apps/hello/views.py
+++ apps/hello/views.py
@@ -1,13 +1,13 @@
 import json
 import logging
-from django.core.urlresolvers import reverse
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render_to_response
 from django.views.decorators.csrf import csrf_protect
 from apps.hello.models import Profile, RequestHistory
+from apps.hello.middleware import exclude_request_tracing
+from apps.hello.utils import get_unreaded_requests_count
 
 from django.conf import settings
-
 
 logger = logging.getLogger(__name__)
 
@@ -21,18 +21,10 @@ def user_detail(request):
 
 def request_list(request):
     latest_requests = RequestHistory.objects\
-        .exclude(path__in=[reverse('hello:ajax_update'),
-                           reverse('hello:ajax_count')])\
         .order_by('-date')[:10]
-    last_request = RequestHistory.objects\
-        .exclude(path__in=[reverse('hello:ajax_update'),
-                           reverse('hello:ajax_count')])\
-        .latest('id')
+    last_request = RequestHistory.objects.latest('id')
 
-    latest_requests_count = 0
-    for request in latest_requests:
-        if not request.is_viewed:
-            latest_requests_count += 1
+    latest_requests_count = get_unreaded_requests_count(latest_requests)
 
     return render_to_response('hello/requests.html',
                               {'latest_requests': latest_requests,
@@ -41,14 +33,13 @@ def request_list(request):
 
 
 @csrf_protect
+@exclude_request_tracing
 def ajax_update(request):
     if request.is_ajax() and request.method == 'POST':
         viewed_request_id = request.POST['viewed']
         try:
             result = RequestHistory.objects\
                 .filter(id__in=viewed_request_id.split(','))\
-                .exclude(path__in=[reverse('hello:ajax_update'),
-                         reverse('hello:ajax_count')])\
                 .update(is_viewed=True)
         except Exception as e:
             logging.info('can\'t update object')
@@ -68,24 +59,17 @@ def ajax_update(request):
 
 
 @csrf_protect
+@exclude_request_tracing
 def ajax_count(request):
     if request.is_ajax() and 'last_loaded_id' in request.POST:
         requests = RequestHistory.objects\
-            .filter(id__gt=request.POST['last_loaded_id'])\
-            .filter(is_viewed=False)\
-            .exclude(path__in=[reverse('hello:ajax_update'),
-                     reverse('hello:ajax_count')])\
-            .all()
+            .order_by('-date')[:10]
 
-        last_request = RequestHistory.objects\
-            .exclude(path__in=[reverse('hello:ajax_update'),
-                               reverse('hello:ajax_count')])\
-            .latest('id')
+        last_request = RequestHistory.objects.latest('id')
 
         data = {'requests': [ob.as_json() for ob in requests],
-                'count': requests.count(),
+                'count': get_unreaded_requests_count(requests),
                 'last_request': last_request.id}
-
         return HttpResponse(json.dumps(data), content_type='application/json')
     return HttpResponse(json.dumps({'response': 'False'}),
                         content_type='application/json')
