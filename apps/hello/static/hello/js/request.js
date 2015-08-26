028b7a8e85eaa9218cec2af2d097ce1a1c9fdb62
unkvuzutop <inkvizitor.dev@gmail.com>
2015-08-27 02:32:32 +0300

    ticket:3 fix review to master

--- apps/hello/static/hello/js/request.js
+++ apps/hello/static/hello/js/request.js
@@ -3,7 +3,7 @@ var csrftoken = $.cookie('csrftoken');
 var titleString = '| 42-unkvuzutop-test';
 
 function csrfSafeMethod(method) {
-    // these HTTP methods do not require CSRF protection
+    // these HTTP methods do not require CSRF protection.
     return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
 }
 $.ajaxSetup({
